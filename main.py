460263bd5a4410d68888aec8b360355398e5cc5e
jamiejamiebobamie <jmccrory@vt.edu>
2020-03-09 15:34:49 -0700

    successfully trained and pickled model in model_trained folder. next to implement and test prediction function.

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ import losses as losses_fn
 from keras.utils import generic_utils
 import roi_helpers as roi_helpers
 
-
-
 import os
 import glob
 from PIL import Image
@@ -47,7 +45,7 @@ wTARGET_SIZE,hTARGET_SIZE = TARGET_SIZE
 wRATIO_BB=wTARGET_RESIZE/wTARGET_SIZE
 hRATIO_BB=hTARGET_RESIZE/hTARGET_SIZE
 RATIO_BB = (wRATIO_BB,hRATIO_BB)
-print(RATIO_BB)
+# print(RATIO_BB)
 
 # https://arxiv.org/abs/1409.1556
 base_model = VGG16(weights='imagenet', include_top=False)
@@ -126,7 +124,7 @@ model_classifier.compile(optimizer=optimizer_classifier,
                          metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
 model_all.compile(optimizer='sgd', loss='mae')
 
-epoch_length = 1000
+epoch_length = 1#1000
 num_epochs = int(cfg.num_epochs)
 iter_num = 0
 
@@ -270,100 +268,3 @@ for epoch_num in range(num_epochs):
             model_all.save_weights(cfg.model_path)
             continue
 print('Training complete, exiting.')
-
-# img_path = (dir_path
-#                     + '/kitti-object-detection/kitti_single/testing/image_2/'
-#                     + '000001.png')
-# img = image.load_img(img_path, target_size=TARGET_RESIZE)
-# x = image.img_to_array(img)
-# x = np.expand_dims(x, axis=0)
-# x = preprocess_input(x)
-# test_feature_map = model.predict(x)
-#
-# print(test_feature_map)
-#
-# input_shape = test_feature_map[0].shape
-# num_classes = 3 # 'Pedestrian', 'Car', 'Background'
-# epochs = 3
-#
-# ANCHOR_BOX_SCALES = [128, 256, 512]
-# ANCHOR_ASPECT_RATIOS = [(1,1), (1,2), (2,1)]
-#
-# # building the RPN
-# # mirrors the architecture of the last 3 layers of VGG16
-# # classification model
-# RPN_class = Sequential()
-# # input layer
-# RPN_class.add(Conv2D(256, kernel_size=(1, 1),
-#                  activation='relu',
-#                  input_shape=input_shape))
-# #block CNN 1
-# RPN_class.add(Conv2D(256, (1, 1), activation='relu'))
-# RPN_class.add(Conv2D(256, (1, 1), activation='relu'))
-# RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
-# #block CNN 2
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
-# # block CNN 3
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
-# # block CNN 4
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# # FC
-# RPN_class.add(Dense(512, activation='relu'))
-# RPN_class.add(Dense(512, activation='relu'))
-# RPN_class.add(Dense(512, activation='relu'))
-# #output layer
-# RPN_class.add(Dense(num_classes, activation='softmax'))
-#
-# # bounding box RPN model
-# RPN_bb = Sequential()
-# # input layer
-# RPN_bb.add(Conv2D(256, kernel_size=(1, 1),
-#                  activation='relu',
-#                  input_shape=input_shape))
-# #block CNN 1
-# RPN_bb.add(Conv2D(256, (1, 1), activation='relu'))
-# RPN_bb.add(Conv2D(256, (1, 1), activation='relu'))
-# RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
-# #block CNN 2
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
-# #block CNN 3
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
-# #block CNN 4
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# # FC
-# RPN_bb.add(Dense(512, activation='relu'))
-# RPN_bb.add(Dense(512, activation='relu'))
-# RPN_bb.add(Dense(512, activation='relu'))
-# # output layer
-# RPN_bb.add(Dense(4, activation='sigmoid')) # 4 numbers for x1,y1,x2,y2 ??
-#
-# # compiling the RPN models.
-# optimizer = keras.optimizers.Adadelta()
-# RPN_bb.compile(loss='mean_squared_error',
-#               optimizer=optimizer,
-#               metrics=['accuracy'])
-# RPN_class.compile(loss='categorical_crossentropy',
-#             optimizer=optimizer,
-#             metrics=['accuracy'])
-#
-# RPN_class.summary()
-# RPN_bb.summary()
-#
-# # example of how to freeze a layer in keras
-# # frozen_layer = Dense(32, trainable=False)
