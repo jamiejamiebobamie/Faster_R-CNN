6c8aca216472cbc2e47f7f1e0122b0d3ec33b5cf
jamiejamiebobamie <jmccrory@vt.edu>
2020-03-09 13:30:20 -0700

    added code

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 
 import keras
+
+from keras import backend as K
+
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Input
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16, preprocess_input
 from sklearn.model_selection import train_test_split
 
+from simple_parser import get_data
+from config import Config
+import data_generators
+import resnet as nn
+
+
 import os
 import glob
 from PIL import Image
 import numpy as np
+
+import pprint
+import random
 
 from utils import print_keras_model_layers
 
@@ -20,10 +32,9 @@ TARGET_SIZE = (1242, 375)
 TARGET_RESIZE = (224, 224)
 
 # the ratio to multiply the bounding box coordinates by to get the
-# new bounding box coordinates of the scaled image.
+# new bounding box coordinates on the scaled image.
 wTARGET_RESIZE,hTARGET_RESIZE = TARGET_RESIZE
 wTARGET_SIZE,hTARGET_SIZE = TARGET_SIZE
-
 wRATIO_BB=wTARGET_RESIZE/wTARGET_SIZE
 hRATIO_BB=hTARGET_RESIZE/hTARGET_SIZE
 RATIO_BB = (wRATIO_BB,hRATIO_BB)
@@ -33,99 +44,318 @@ print(RATIO_BB)
 base_model = VGG16(weights='imagenet', include_top=False)
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('block2_pool').output)
 
-img_path = (dir_path
-                    + '/kitti-object-detection/kitti_single/testing/image_2/'
-                    + '000001.png')
-img = image.load_img(img_path, target_size=TARGET_RESIZE)
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-test_feature_map = model.predict(x)
+cfg = Config()
+cfg.use_horizontal_flips = True
+cfg.use_vertical_flips = True
+cfg.rot_90 = True
+cfg.num_rois = 32
+cfg.base_net_weights = model.get_weights()
 
-print(test_feature_map)
+all_images, classes_count, class_mapping = get_data('dataset.txt')
 
-input_shape = test_feature_map[0].shape
-num_classes = 3 # 'Pedestrian', 'Car', 'Background'
-epochs = 3
+if 'bg' not in classes_count:
+    classes_count['bg'] = 0
+    class_mapping['bg'] = len(class_mapping)
 
-ANCHOR_BOX_SCALES = [128, 256, 512]
-ANCHOR_ASPECT_RATIOS = [(1,1), (1,2), (2,1)]
+cfg.class_mapping = class_mapping
 
-# building the RPN
-# mirrors the architecture of the last 3 layers of VGG16
-# classification model
-RPN_class = Sequential()
-# input layer
-RPN_class.add(Conv2D(256, kernel_size=(1, 1),
-                 activation='relu',
-                 input_shape=input_shape))
-#block CNN 1
-RPN_class.add(Conv2D(256, (1, 1), activation='relu'))
-RPN_class.add(Conv2D(256, (1, 1), activation='relu'))
-RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
-#block CNN 2
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
-# block CNN 3
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
-# block CNN 4
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
-# FC
-RPN_class.add(Dense(512, activation='relu'))
-RPN_class.add(Dense(512, activation='relu'))
-RPN_class.add(Dense(512, activation='relu'))
-#output layer
-RPN_class.add(Dense(num_classes, activation='softmax'))
+print('Training images per class:')
+pprint.pprint(classes_count)
+print('Num classes (including bg) = {}'.format(len(classes_count)))
+random.shuffle(all_images)
 
-# bounding box RPN model
-RPN_bb = Sequential()
-# input layer
-RPN_bb.add(Conv2D(256, kernel_size=(1, 1),
-                 activation='relu',
-                 input_shape=input_shape))
-#block CNN 1
-RPN_bb.add(Conv2D(256, (1, 1), activation='relu'))
-RPN_bb.add(Conv2D(256, (1, 1), activation='relu'))
-RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
-#block CNN 2
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
-#block CNN 3
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
-#block CNN 4
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
-# FC
-RPN_bb.add(Dense(512, activation='relu'))
-RPN_bb.add(Dense(512, activation='relu'))
-RPN_bb.add(Dense(512, activation='relu'))
-# output layer
-RPN_bb.add(Dense(4, activation='sigmoid')) # 4 numbers for x1,y1,x2,y2 ??
+num_imgs = len(all_images)
+train_imgs = [s for s in all_images if s['imageset'] == 'trainval']
+val_imgs = [s for s in all_images if s['imageset'] == 'test']
 
-# compiling the RPN models.
-optimizer = keras.optimizers.Adadelta()
-RPN_bb.compile(loss='mean_squared_error',
-              optimizer=optimizer,
-              metrics=['accuracy'])
-RPN_class.compile(loss='categorical_crossentropy',
-            optimizer=optimizer,
-            metrics=['accuracy'])
+print('Num train samples {}'.format(len(train_imgs)))
+print('Num val samples {}'.format(len(val_imgs)))
 
-RPN_class.summary()
-RPN_bb.summary()
 
-# example of how to freeze a layer in keras
-# frozen_layer = Dense(32, trainable=False)
+data_gen_train = data_generators.get_anchor_gt(train_imgs, classes_count, cfg, nn.get_img_output_length,
+                                               K.image_dim_ordering(), mode='train')
+data_gen_val = data_generators.get_anchor_gt(val_imgs, classes_count, cfg, nn.get_img_output_length,
+                                             K.image_dim_ordering(), mode='val')
+
+if K.image_dim_ordering() == 'th':
+    input_shape_img = (3, None, None)
+else:
+    input_shape_img = (None, None, 3)
+
+img_input = Input(shape=input_shape_img)
+roi_input = Input(shape=(None, 4))
+
+# define the base network (resnet here, can be VGG, Inception, etc)
+shared_layers = nn.nn_base(img_input, trainable=True)
+
+# define the RPN, built on the base layers
+num_anchors = len(cfg.anchor_box_scales) * len(cfg.anchor_box_ratios)
+rpn = nn.rpn(shared_layers, num_anchors)
+
+classifier = nn.classifier(shared_layers, roi_input, cfg.num_rois, nb_classes=len(classes_count), trainable=True)
+
+model_rpn = Model(img_input, rpn[:2])
+model_classifier = Model([img_input, roi_input], classifier)
+
+# this is a model that holds both the RPN and the classifier, used to load/save weights for the models
+model_all = Model([img_input, roi_input], rpn[:2] + classifier)
+
+try:
+    print('loading weights from {}'.format(cfg.base_net_weights))
+    model_rpn.load_weights(cfg.model_path, by_name=True)
+    model_classifier.load_weights(cfg.model_path, by_name=True)
+except Exception as e:
+    print(e)
+    print('Could not load pretrained model weights. Weights can be found in the keras application folder '
+          'https://github.com/fchollet/keras/tree/master/keras/applications')
+
+optimizer = Adam(lr=1e-5)
+optimizer_classifier = Adam(lr=1e-5)
+model_rpn.compile(optimizer=optimizer,
+                  loss=[losses_fn.rpn_loss_cls(num_anchors), losses_fn.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=optimizer_classifier,
+                         loss=[losses_fn.class_loss_cls, losses_fn.class_loss_regr(len(classes_count) - 1)],
+                         metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+model_all.compile(optimizer='sgd', loss='mae')
+
+epoch_length = 1000
+num_epochs = int(cfg.num_epochs)
+iter_num = 0
+
+losses = np.zeros((epoch_length, 5))
+rpn_accuracy_rpn_monitor = []
+rpn_accuracy_for_epoch = []
+start_time = time.time()
+
+best_loss = np.Inf
+
+class_mapping_inv = {v: k for k, v in class_mapping.items()}
+print('Starting training')
+
+vis = True
+
+for epoch_num in range(num_epochs):
+
+    progbar = generic_utils.Progbar(epoch_length)
+    print('Epoch {}/{}'.format(epoch_num + 1, num_epochs))
+
+    while True:
+        try:
+
+            if len(rpn_accuracy_rpn_monitor) == epoch_length and cfg.verbose:
+                mean_overlapping_bboxes = float(sum(rpn_accuracy_rpn_monitor)) / len(rpn_accuracy_rpn_monitor)
+                rpn_accuracy_rpn_monitor = []
+                print(
+                    'Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(
+                        mean_overlapping_bboxes, epoch_length))
+                if mean_overlapping_bboxes == 0:
+                    print('RPN is not producing bounding boxes that overlap'
+                          ' the ground truth boxes. Check RPN settings or keep training.')
+
+            X, Y, img_data = next(data_gen_train)
+
+            loss_rpn = model_rpn.train_on_batch(X, Y)
+
+            P_rpn = model_rpn.predict_on_batch(X)
+
+            result = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], cfg, K.image_dim_ordering(), use_regr=True,
+                                            overlap_thresh=0.7,
+                                            max_boxes=300)
+            # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
+            X2, Y1, Y2, IouS = roi_helpers.calc_iou(result, img_data, cfg, class_mapping)
+
+            if X2 is None:
+                rpn_accuracy_rpn_monitor.append(0)
+                rpn_accuracy_for_epoch.append(0)
+                continue
+
+            neg_samples = np.where(Y1[0, :, -1] == 1)
+            pos_samples = np.where(Y1[0, :, -1] == 0)
+
+            if len(neg_samples) > 0:
+                neg_samples = neg_samples[0]
+            else:
+                neg_samples = []
+
+            if len(pos_samples) > 0:
+                pos_samples = pos_samples[0]
+            else:
+                pos_samples = []
+
+            rpn_accuracy_rpn_monitor.append(len(pos_samples))
+            rpn_accuracy_for_epoch.append((len(pos_samples)))
+
+            if cfg.num_rois > 1:
+                if len(pos_samples) < cfg.num_rois // 2:
+                    selected_pos_samples = pos_samples.tolist()
+                else:
+                    selected_pos_samples = np.random.choice(pos_samples, cfg.num_rois // 2, replace=False).tolist()
+                try:
+                    selected_neg_samples = np.random.choice(neg_samples, cfg.num_rois - len(selected_pos_samples),
+                                                            replace=False).tolist()
+                except:
+                    selected_neg_samples = np.random.choice(neg_samples, cfg.num_rois - len(selected_pos_samples),
+                                                            replace=True).tolist()
+
+                sel_samples = selected_pos_samples + selected_neg_samples
+            else:
+                # in the extreme case where num_rois = 1, we pick a random pos or neg sample
+                selected_pos_samples = pos_samples.tolist()
+                selected_neg_samples = neg_samples.tolist()
+                if np.random.randint(0, 2):
+                    sel_samples = random.choice(neg_samples)
+                else:
+                    sel_samples = random.choice(pos_samples)
+
+            loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]],
+                                                         [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
+
+            losses[iter_num, 0] = loss_rpn[1]
+            losses[iter_num, 1] = loss_rpn[2]
+
+            losses[iter_num, 2] = loss_class[1]
+            losses[iter_num, 3] = loss_class[2]
+            losses[iter_num, 4] = loss_class[3]
+
+            iter_num += 1
+
+            progbar.update(iter_num,
+                           [('rpn_cls', np.mean(losses[:iter_num, 0])), ('rpn_regr', np.mean(losses[:iter_num, 1])),
+                            ('detector_cls', np.mean(losses[:iter_num, 2])),
+                            ('detector_regr', np.mean(losses[:iter_num, 3]))])
+
+            if iter_num == epoch_length:
+                loss_rpn_cls = np.mean(losses[:, 0])
+                loss_rpn_regr = np.mean(losses[:, 1])
+                loss_class_cls = np.mean(losses[:, 2])
+                loss_class_regr = np.mean(losses[:, 3])
+                class_acc = np.mean(losses[:, 4])
+
+                mean_overlapping_bboxes = float(sum(rpn_accuracy_for_epoch)) / len(rpn_accuracy_for_epoch)
+                rpn_accuracy_for_epoch = []
+
+                if cfg.verbose:
+                    print('Mean number of bounding boxes from RPN overlapping ground truth boxes: {}'.format(
+                        mean_overlapping_bboxes))
+                    print('Classifier accuracy for bounding boxes from RPN: {}'.format(class_acc))
+                    print('Loss RPN classifier: {}'.format(loss_rpn_cls))
+                    print('Loss RPN regression: {}'.format(loss_rpn_regr))
+                    print('Loss Detector classifier: {}'.format(loss_class_cls))
+                    print('Loss Detector regression: {}'.format(loss_class_regr))
+                    print('Elapsed time: {}'.format(time.time() - start_time))
+
+                curr_loss = loss_rpn_cls + loss_rpn_regr + loss_class_cls + loss_class_regr
+                iter_num = 0
+                start_time = time.time()
+
+                if curr_loss < best_loss:
+                    if cfg.verbose:
+                        print('Total loss decreased from {} to {}, saving weights'.format(best_loss, curr_loss))
+                    best_loss = curr_loss
+                    model_all.save_weights(cfg.model_path)
+
+                break
+
+        except Exception as e:
+            print('Exception: {}'.format(e))
+            # save model
+            model_all.save_weights(cfg.model_path)
+            continue
+print('Training complete, exiting.')
+
+# img_path = (dir_path
+#                     + '/kitti-object-detection/kitti_single/testing/image_2/'
+#                     + '000001.png')
+# img = image.load_img(img_path, target_size=TARGET_RESIZE)
+# x = image.img_to_array(img)
+# x = np.expand_dims(x, axis=0)
+# x = preprocess_input(x)
+# test_feature_map = model.predict(x)
+#
+# print(test_feature_map)
+#
+# input_shape = test_feature_map[0].shape
+# num_classes = 3 # 'Pedestrian', 'Car', 'Background'
+# epochs = 3
+#
+# ANCHOR_BOX_SCALES = [128, 256, 512]
+# ANCHOR_ASPECT_RATIOS = [(1,1), (1,2), (2,1)]
+#
+# # building the RPN
+# # mirrors the architecture of the last 3 layers of VGG16
+# # classification model
+# RPN_class = Sequential()
+# # input layer
+# RPN_class.add(Conv2D(256, kernel_size=(1, 1),
+#                  activation='relu',
+#                  input_shape=input_shape))
+# #block CNN 1
+# RPN_class.add(Conv2D(256, (1, 1), activation='relu'))
+# RPN_class.add(Conv2D(256, (1, 1), activation='relu'))
+# RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
+# #block CNN 2
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
+# # block CNN 3
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(MaxPooling2D(pool_size=(2, 2)))
+# # block CNN 4
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_class.add(Conv2D(512, (1, 1), activation='relu'))
+# # FC
+# RPN_class.add(Dense(512, activation='relu'))
+# RPN_class.add(Dense(512, activation='relu'))
+# RPN_class.add(Dense(512, activation='relu'))
+# #output layer
+# RPN_class.add(Dense(num_classes, activation='softmax'))
+#
+# # bounding box RPN model
+# RPN_bb = Sequential()
+# # input layer
+# RPN_bb.add(Conv2D(256, kernel_size=(1, 1),
+#                  activation='relu',
+#                  input_shape=input_shape))
+# #block CNN 1
+# RPN_bb.add(Conv2D(256, (1, 1), activation='relu'))
+# RPN_bb.add(Conv2D(256, (1, 1), activation='relu'))
+# RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
+# #block CNN 2
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
+# #block CNN 3
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(MaxPooling2D(pool_size=(2, 2)))
+# #block CNN 4
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# RPN_bb.add(Conv2D(512, (1, 1), activation='relu'))
+# # FC
+# RPN_bb.add(Dense(512, activation='relu'))
+# RPN_bb.add(Dense(512, activation='relu'))
+# RPN_bb.add(Dense(512, activation='relu'))
+# # output layer
+# RPN_bb.add(Dense(4, activation='sigmoid')) # 4 numbers for x1,y1,x2,y2 ??
+#
+# # compiling the RPN models.
+# optimizer = keras.optimizers.Adadelta()
+# RPN_bb.compile(loss='mean_squared_error',
+#               optimizer=optimizer,
+#               metrics=['accuracy'])
+# RPN_class.compile(loss='categorical_crossentropy',
+#             optimizer=optimizer,
+#             metrics=['accuracy'])
+#
+# RPN_class.summary()
+# RPN_bb.summary()
+#
+# # example of how to freeze a layer in keras
+# # frozen_layer = Dense(32, trainable=False)
